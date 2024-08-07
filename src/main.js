db58f19b6d77f26627e5722c79afdbb145414a06
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2024-08-07 13:37:42 +0800

    Create intro, about section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import '../node_modules/modern-normalize/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/components/header.css';
+import '../styles/components/intro.css';
+import '../styles/components/about.css';
 import '../styles/ultis.css';
 
