585f8e09f325f7022a73959c970563d450f4aa3d
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2024-08-21 16:56:06 +0800

    Create featured section

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/intro.css';
 import '../styles/components/about.css';
-import '../styles/components/education.css'
+import '../styles/components/education.css';
+import '../styles/components/featured.css';
 import '../styles/ultis.css';
 
