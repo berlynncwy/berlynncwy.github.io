94d07e3a1646ca433c88baa7eace871e0bc9ee3a
Cantius Chew <cantiuschew96@hotmail.com>
2024-12-17 22:34:08 +0800

    add modern-normalize.css to stylesheet; add stylesheets to html

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import '../node_modules/modern-normalize/modern-normalize.css';
-import '../styles/style.css';
-import '../styles/components/header.css';
-import '../styles/components/intro.css';
-import '../styles/components/about.css';
-import '../styles/components/education.css';
-import '../styles/components/featured.css';
-import '../styles/components/contact.css';
-import '../styles/components/footer.css';
-import '../styles/components/mobile-nav.css';
-import '../styles/utils.css';
-
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../styles/components/header.css";
+import "../styles/components/intro.css";
+import "../styles/components/about.css";
+import "../styles/components/education.css";
+import "../styles/components/featured.css";
+import "../styles/components/contact.css";
+import "../styles/components/footer.css";
+import "../styles/components/mobile-nav.css";
+import "../styles/utils.css";
